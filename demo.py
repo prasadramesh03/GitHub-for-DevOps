cda6d7c8be33c68212bbc87acd5f2bc09e6ac52b
prasadramesh03 <prasadramesh963@gmail.com>
2024-12-02 16:27:13 +0530

    added testing

--- demo.py
+++ demo.py
@@ -1,10 +1,15 @@
+"""
+This is a simple demo module that defines a function to return a value.
+"""
+
+
 def my_function():
+    """
+    This function returns the value 5.
+    """
     a = 5
-    b = 6
-
-    return d 
-        
-my_function()
+    return a
 
 
-test
+result = my_function()  # Call the function and store the result
+print(result)  # Print the result to demonstrate the function call
